2c320add8341698b029d02666420cbc51fdd8976
dreezy10 <demba10@live.com.pt>
2019-08-26 13:47:38 +0000

    basic configuration all done.

---
+++ guinevisitaturisticabackend/src/main/java/com/guinevisita/guinevisitaturisticabackend/App.java
@@ -0,0 +1,13 @@
+package com.guinevisita.guinevisitaturisticabackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
